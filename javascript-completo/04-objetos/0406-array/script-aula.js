b5ffc1606e199f10b8dc525f79ba8026c83e613d
Gabriel Chagas <gabrielserq@outlook.com>
2022-02-20 12:51:21 -0300

    chore: adding script-aula.js to the project.

--- javascript-completo/04-objetos/0406-array/script-aula.js
+++ javascript-completo/04-objetos/0406-array/script-aula.js
@@ -1,78 +1,35 @@
-// const instrumentos = ['Guitarra', 'Baixo', 'Violão'];
-// const precos = [49, 99, 69, 89];
+const instrumentos = ['Guitarra', 'Baixo', 'Violão'];
+const precos = [49, 99, 69, 89];
 
-// const dados = [new String('Tipo 1'), ['Carro', 'Portas', {cor: 'Azul', preco: 2000}], function andar(nome) { console.log(nome) }];
+const dados = [new String('Tipo 1'), ['Carro', 'Portas', {cor: 'Azul', preco: 2000}], function andar(nome) { console.log(nome) }];
 
-// const carros = new Array('Ford', 'Fiat', 'Honda');
-
-// carros[2] = 'Ferrari';
-// carros[3] = 'Kia';
-// carros[20] = 'Kia';
-
-// console.log(carros.length)
-
-// const li = document.querySelectorAll('li');
-
-// const arrayLi = Array.from(li)
-
-// const obj = {
-//   0: 'Andre',
-//   1: 'Rafael',
-//   2: 'Teste',
-//   length: 4,
-// }
-
-// const objArray = Array.from(obj);
-
-// console.log(li)
-// console.log(arrayLi)
-
-// const frutas = ['Banana', 'Pêra', ['Uva Roxa', 'Uva Verde']];
-
-// console.log(frutas[2][0].length)
-
-// const instrumentos = ['Guitarra', 'Baixo', 'Violão'];
-// instrumentos.sort();
-
-// const idades = [32,21,33,43,1,12,8];
-// idades.sort();
-
-// console.log(instrumentos)
-// console.log(idades)
-
-const carros = ['Ford', 'Fiat', 'VW'];
-carros.unshift('Kia', 'Ferrari');
-const novaArray = carros.push('Parati', 'Gol');
-
-// console.log(novaArray)
-
-// console.log(carros);
-
-// console.log(carros.shift())
-
-// console.log(carros);
-// console.log(carros.splice(2,4,'Fusca'))
-
-// console.log(carros);
-
-// console.log(['Item1', 'Item2', 'Item3', 'Item4', 'item5'].copyWithin(2, 0, 2));
-
-// console.log(['Item1', 'Item2', 'Item3', 'Item4'].fill('Banana', 0, 3));
-
-// const transporte1 = ['Barco', 'Aviao'];
-// const transporte2 = ['Carro', 'Moto'];
+dados[2]('Ford');
+dados[1][2].cor; // azul
 
 
-// let htmlString = '<h2>Título Principal</h2>';
-// htmlString = htmlString.split('h2');
-// htmlString = htmlString.join('h1')
+const carros = {
+  0: 'Fiat',
+  1: 'Honda',
+  2: 'Ford',
+  length: 3,
+}
 
-// console.log(htmlString)
+const carrosArray = Array.from(carros);
 
-const linguagens = ['html', 'css', 'js', 'php', 'python'];
+let li = document.querySelectorAll('li'); // NodeList
+console.log(Array.isArray(li)); // false
 
-const cloneLinguagens = linguagens.slice();
+li = Array.from(li); // Array
+console.log(Array.isArray(li))// true
 
-console.log(linguagens.pop())
-console.log(linguagens)
-console.log(cloneLinguagens)
+const frutas = ['Banana', 'Pêra', ['Uva Roxa', 'Uva Verde']];
+console.log(frutas.length); // 3
+
+console.log(frutas[0].length); // 6
+console.log(frutas[1].length); // 4
+console.log(frutas[2].length); // 2
+
+
+console.log(instrumentos.sort())
+const idades = [32,21,33,43,1,12,8];
+console.log(idades.sort());
