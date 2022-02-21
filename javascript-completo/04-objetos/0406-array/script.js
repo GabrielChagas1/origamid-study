ee0cdd1cdf0850d23004adb1c159df537ced047a
Gabriel Chagas <gabrielserq@outlook.com>
2022-02-21 08:54:00 -0300

    chore: exercices for fixed

---
+++ javascript-completo/04-objetos/0406-array/script.js
@@ -0,0 +1,42 @@
+const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
+// Remova o primeiro valor de comidas e coloque em uma variável
+// Remova o último valor de comidas e coloque em uma variável
+// Adicione 'Arroz' ao final da array
+// Adicione 'Peixe' e 'Batata' ao início da array
+
+let firstFood = comidas.shift();
+let lastFood = comidas.pop();
+
+comidas.push('Arroz');
+comidas.unshift('Peixe', 'Batata')
+
+
+
+const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
+// Arrume os estudantes em ordem alfabética
+// Inverta a ordem dos estudantes
+// Verifique se Joana faz parte dos estudantes
+// Verifique se Juliana faz parte dos estudantes
+
+const OrderEstudents = estudantes.sort();
+const OrderEstudentsInvert = estudantes.reverse();
+const isJoana = estudantes.includes('Joana');
+const isJuliana = estudantes.includes('Juliana');
+
+
+let html = `<section>
+              <div>Sobre</div>
+              <div>Produtos</div>
+              <div>Contato</div>
+            </section>`
+// Substitua section por ul e div com li,
+// utilizando split e join
+html = html.split('section').join('ul').split('div').join('li');
+console.log(html);
+
+const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+// Remova o último carro, mas antes de remover
+// salve a array original em outra variável
+
+const saveCars = carros.slice();
+saveCars.pop();
