30320012e329544733d8f5132f1558f975558199
Gabriel Chagas <gabrielserq@outlook.com>
2022-02-21 08:53:44 -0300

    chore: annotations class

--- javascript-completo/04-objetos/0406-array/script-aula.js
+++ javascript-completo/04-objetos/0406-array/script-aula.js
@@ -33,3 +33,31 @@ console.log(frutas[2].length); // 2
 console.log(instrumentos.sort())
 const idades = [32,21,33,43,1,12,8];
 console.log(idades.sort());
+
+
+carrosArray.splice(0, 2, "Tesla")
+
+console.log(['Item1', 'Item2', 'Item3', 'Item4'].copyWithin(2, 0, 3))
+// ['Item1', 'Item2', 'Item1', 'Item2']
+
+console.log(['Item1', 'Item2', 'Item3', 'Item4'].copyWithin(-1));
+// ['Item1', 'Item2', 'Item3', 'Item1']
+
+const transporte1 = ['Barco', 'Aviao'];
+const transporte2 = ['Carro', 'Moto'];
+const transportes = transporte1.concat(transporte2);
+// ['Barco', 'Aviao', 'Carro', 'Moto'];
+
+const maisTransportes = [].concat(transporte1, transporte2, 'Van');
+// ['Barco', 'Aviao', 'Carro', 'Moto', 'Van'];
+
+
+const linguagens = ['html', 'css', 'js', 'php', 'python', 'js'];
+
+linguagens.includes('css'); // true
+linguagens.includes('ruby'); // false
+linguagens.indexOf('python'); // 4
+linguagens.indexOf('js'); // 2
+linguagens.lastIndexOf('js'); // 5
+
+const cloneLinguagens = linguagens.slice(0, 8);
